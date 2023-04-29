c056d1804cef244baa0f20d17655a7bbf47cbfaf
Nishchal Jatwani <nishchaljatwani@Nishchals-MacBook-Pro.local>
2023-04-29 17:05:20 -0400

    chore: homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import { Navbar } from './components/Navbar';
+import { Box } from '@mui/material';
+
 
 function App() {
   return (
+    <>
     <Navbar/>
+      <Box
+        component="img"
+        sx={{
+          height: '100vh',
+          width: '100vw',
+          objectFit: 'cover',
+        }}
+        alt="The house from the offer."
+        src="https://images.unsplash.com/photo-1515404929826-76fff9fef6fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80"
+      />
+    </>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,31 @@
 import React from "react";
-import { Toolbar, Typography,AppBar,IconButton, colors, Button } from "@mui/material";
-import {Divider} from "@mui/material";
-
+import { Toolbar, Typography,AppBar,IconButton, colors, Button, Switch, Box, Icon } from "@mui/material";
+import {Stack} from "@mui/material";
+import WavesIcon from '@mui/icons-material/Waves';
 
 export const Navbar = () => {
     return (
+        <AppBar color="inherit">
         <Toolbar>
-            <Button variant = "text" color = "inherit" size ="large">Home</Button>
-            <Divider variant="middle"/>
-        </Toolbar>
-
-    )
+        <Stack spacing={1} direction="row">
+        <Button variant="text" color="inherit" size="medium" disableTouchRipple>
+            <WavesIcon />
+          </Button>
+          <Button variant="text" color="inherit" size="medium" >
+            About
+          </Button>
+          <Button variant="text" color="inherit" size="medium" >
+            Stays
+          </Button>
+          <Button variant="text" color="inherit" size="medium" >
+            Experience
+          </Button>
+        </Stack>
+        <Stack direction="row" style={{ marginLeft: "auto" }}>
+            <Button variant ="outlined" color = "inherit" size="medium" >Book Now</Button>
+        </Stack>
+      </Toolbar>
+     </AppBar>
+      
+    );
 }
