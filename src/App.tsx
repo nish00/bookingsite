51aa290bda1aaba677c64a4357761d873fa2c2ad
Nishchal Jatwani <nishchaljatwani@Nishchals-MacBook-Pro.local>
2023-05-02 12:07:37 -0400

    modify: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         sx={{
           height: '100vh',
           width: '100vw',
-          objectFit: 'cover',
+          objectFit: 'cover',~
         }}
         alt="The house from the offer."
         src="https://images.unsplash.com/photo-1515404929826-76fff9fef6fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80"
